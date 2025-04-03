630bb3449fe62eb5dd82f0c62d4356e49df67b7c
Vaulther <97086760@st.deltion.nl>
2025-04-03 14:30:36 +0200

    040 | Opdracht 2 gemaakt

--- opdrachten/040_lists/opdr_2/opdr_2.py
+++ opdrachten/040_lists/opdr_2/opdr_2.py
@@ -13,3 +13,16 @@ rivieren = list(rivier_info.keys())
 # rivieren is nu een list met alleen de riviernamen: ['rijn', 'maas', 'nijl']
 
 # Hier jouw code.....
+
+rivier_info = { 
+    "rijn": ["nederland", "duitsland", "Frankrijk"], 
+    "maas": ["nederland", "belgië", "duitsland"], 
+    "nijl": ["egypte", "soedan", "oeganda"] 
+}
+rivieren = list(rivier_info.keys())
+
+for rivier in rivieren:
+    rivier_naam = rivier.capitalize()
+    land = rivier_info[rivier][2].capitalize()
+    print(f"De Rivier {rivier_naam} stroomt onder andere door {land}")
+
