87ef350e15263dbcfa47997f6ec2da59b2d492e1
Vaulther <97086760@st.deltion.nl>
2025-04-03 13:53:23 +0200

    030 | Opdracht 2 en 3 gemaakt

--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,20 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
+c1 = -12
+f1 = 102
 
-print()
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+c2 = 62
+f2 = -2
+
+print(f"{c1} graden Celsius is gelijk aan {celsius_to_fahrenheit(c1):.1f} graden Fahrenheit")
+print(f"{f1} graden Fahrenheit is gelijk aan {fahrenheit_to_celsius(f1):.1f} graden Celsius")
+
+print(f"{c2} graden Celsius is gelijk aan {celsius_to_fahrenheit(c2):.1f} graden Fahrenheit")
+print(f"{f2} graden Fahrenheit is gelijk aan {fahrenheit_to_celsius(f2):.1f} graden Celsius")
+
--- opdrachten/030_berekeningen/opdr_3/opdr_3.py
+++ opdrachten/030_berekeningen/opdr_3/opdr_3.py
@@ -3,14 +3,22 @@
 # Groep:
 
 # Hier komt je code...
-x = 1
-y = ...
-print( ... )
+def calculate_y(x):
+    return 4 * (x ** 3) - 2 * (x ** 2) + 5 * x - 2
 
-x = 2
-y = ...
-print( ... )
+# Test de functie met de gegeven waarden van x
+x_values = [0, 1, -1, 25]
+for x in x_values:
+    y = calculate_y(x)
+    print(f"x = {x}, y = {y}")
 
-x = 0
-y = ...
-print( ... )
+# Wissel de waarden van a en b
+a = "b"
+b = "a"
+a, b = b, a
+
+print(f"a is: {a}")
+print(f"b is: {b}")
+
+
+
