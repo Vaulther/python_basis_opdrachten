4ddaedfb562f74d622bcebb355edc981b7820637
Vaulther <97086760@st.deltion.nl>
2025-04-03 14:04:53 +0200

    040 | Opdracht 1 gemaakt

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,16 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+mylist = []
+
+dict0 = {'id': 0, 'voornaam': 'Dylan', 'achternaam': 'Joost'}
+dict1 = {'id': 1, 'voornaam': 'Emma', 'achternaam': 'Van Dijk'}
+dict2 = {'id': 2, 'voornaam': 'Lucas', 'achternaam': 'De Vries'}
+dict3 = {'id': 3, 'voornaam': 'Sophie', 'achternaam': 'Bakker'}
+
+mylist.append(dict0)
+mylist.append(dict1)
+mylist.append(dict2)
+mylist.append(dict3)
+
+print(mylist[1])
