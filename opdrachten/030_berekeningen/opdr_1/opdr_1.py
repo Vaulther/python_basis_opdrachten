e3c982ecabac0ec15430e1d72a3d69873ba1021c
Vaulther <97086760@st.deltion.nl>
2025-04-03 09:56:21 +0200

    030 | Opdracht 1 gemaakt

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,15 @@
 # Groep:
 
 # Hier komt je code...
+Zanto = 21
+Julia = 22
+Levi = 23
+
+Totale_leeftijd = Zanto + Julia + Levi
+Gemiddelde_leeftijd = Totale_leeftijd / 3
+print("Dit weten we over Zanto, Julia en Levi:")
+print("De totale waarde van alle leeftijden bij elkaar opgeteld:", Totale_leeftijd)
+print("De gemiddelde leeftijd van deze vrienden is:", Gemiddelde_leeftijd)
+
+
+
